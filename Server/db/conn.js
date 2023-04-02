bac6b355f6753efffff8ab9210ec7d5fc8480b6c
harshit <harshit302001n@gmail.com>
2023-04-03 00:37:59 +0530

    first commit

---
+++ Server/db/conn.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+const db = process.env.DATABASE;
+mongoose.connect(db).then(()=>{
+    console.log('Database is connected');
+
+}).catch((err)=>{
+    console.log('Database is not connected'+err);
+})
