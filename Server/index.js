a2ce21978d59c475c6bc8ad4b0d48a1b3ca9e433
harshit <harshit302001n@gmail.com>
2023-04-05 13:28:55 +0530

    second commit

--- Server/index.js
+++ Server/index.js
@@ -10,13 +10,13 @@ require('./db/conn')
 app.use(express.json())
 app.use(require('./router/router'))
 
-// if(process.env.NODE_ENV=='production'){
-//     app.use(express.static('client/build'));
-//     const path = require('path')
-//     app.get('*',(req,res)=>{
-//         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-//     })
-// }
+ if(process.env.NODE_ENV=='production'){
+     app.use(express.static('client/build'));
+     const path = require('path')
+     app.get('*',(req,res)=>{
+         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+     })
+ }
 
 app.listen(PORT,()=>{
     console.log('Server is onpen at localhost'+PORT)
